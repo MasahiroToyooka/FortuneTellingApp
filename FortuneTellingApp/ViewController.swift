2e2eb6fb8d02734d83c0f17af1bf22f3cb520115
MasahiroToyooka <ehara2481@gmail.com>
2019-08-08 19:30:47 +0900

    コメントの記入

--- FortuneTellingApp/ViewController.swift
+++ FortuneTellingApp/ViewController.swift
@@ -10,17 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    /// 生年月日を入力するピッカー
     @IBOutlet weak var dataPicker: UIDatePicker!
     
+    /// 血液型を入力するピッカー
     @IBOutlet weak var bloodsegment: UISegmentedControl!
     
+    /// スライダーの数字を反映するラベル
     @IBOutlet weak var numLabel: UILabel!
     
+    /// 占い結果を表示するテキストビュー
     @IBOutlet weak var resultTextView: UITextView!
     
+    // スライダーの値を格納する変数
     var sliderNum: Int = 50
     
-    
+    // ピッカーの値を変えたときに変更する配列の占いのデータ
     let pickerData: [String] = [
         "あなたは山に愛されています。\n",
         "あなたは海に愛されています。\n",
@@ -28,6 +33,7 @@ class ViewController: UIViewController {
         "あなたは森に愛されています。\n",
     ]
     
+    // 血液型を変えたときに変更する配列の占いのデータ
     let bloodData: [String] = [
         "属性は「雷」です。\n",
         "属性は「水」です。\n",
@@ -35,6 +41,7 @@ class ViewController: UIViewController {
         "属性は「草」です。\n",
     ]
     
+    // スライダーで好きな数字をかえたときに変更する配列の占いのデータ
     let sliderData: [String] = [
         "努力が報われなかったときは、「自分へのごほうび」とつぶやきながら、ひと口サイズのスイーツを口にして。",
         "探し物を思い浮かべながら、人差し指を7回反時計回りにまわしてみて。",
@@ -47,16 +54,16 @@ class ViewController: UIViewController {
         
     }
     
-    fileprivate func setupTextView(picikerNum: Int, bloodNum: Int, sliderNum: Int) -> String {
-        return pickerData[picikerNum] + bloodData[bloodNum] + sliderData[sliderNum]
-    }
-   
+    // スライダーが呼ばれた時の処理
     @IBAction func numSlider(_ sender: UISlider) {
+        // sliderNumにInt型の値を代入
         sliderNum = Int(sender.value)
         
+        // numLabelのテキストを反映
         numLabel.text = String(sliderNum)
     }
     
+    // Pickerに入れられた値から配列で使うインデックスを返す関数
     fileprivate func setPickerNum() -> Int {
         let date = dataPicker.date
         
@@ -69,18 +76,27 @@ class ViewController: UIViewController {
         return num
     }
     
+    // bloodsegmentに入れられた値から配列で使うインデックスを返す関数
     fileprivate func setBloodNum() -> Int{
         let num = bloodsegment.selectedSegmentIndex
         return num
     }
     
+    // numSliderに入れられた値から配列で使うインデックスを返す関数
     fileprivate func setSliderNum() -> Int {
         let num = (sliderNum % 4)
         return num
     }
     
+    // resultTextViewに表示する値を決める関数
+    fileprivate func setupTextView(picikerNum: Int, bloodNum: Int, sliderNum: Int) -> String {
+        return pickerData[picikerNum] + bloodData[bloodNum] + sliderData[sliderNum]
+    }
+    
+    // 占うボタンが押された時に呼び出す処理
     @IBAction func fortuneButton(_ sender: UIButton) {
         
+        // resultTextViewにテキストにsetupTextView関数を呼び出してテキストを代入
         resultTextView.text = setupTextView(picikerNum: setPickerNum(), bloodNum: setBloodNum(), sliderNum: setSliderNum())
     }
 }
