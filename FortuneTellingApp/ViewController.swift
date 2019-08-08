263d8d5e9803235ed13e0cfd5cec0b23d56bacd8
MasahiroToyooka <ehara2481@gmail.com>
2019-08-08 13:05:07 +0900

    占いデータの格納

--- FortuneTellingApp/ViewController.swift
+++ FortuneTellingApp/ViewController.swift
@@ -18,10 +18,37 @@ class ViewController: UIViewController {
     
     var sliderNum: Int = 50
     
+    
+    let pickerData: [String] = [
+        "あなたは山に愛されています。\n",
+        "あなたは海に愛されています。\n",
+        "あなたは川に愛されています。\n",
+        "あなたは森に愛されています。\n",
+    ]
+    
+    let bloodData: [String] = [
+        "属性は「雷」です。\n",
+        "属性は「水」です。\n",
+        "属性は「風」です。\n",
+        "属性は「草」です。\n",
+        "属性は「岩」です。\n",
+    ]
+    
+    let numData: [String] = [
+        "努力が報われなかったときは、「自分へのごほうび」とつぶやきながら、ひと口サイズのスイーツを口にして。",
+        "探し物を思い浮かべながら、人差し指を7回反時計回りにまわしてみて。",
+        "銀製品や銀のアクセサリーをピカピカに磨いてから、バッグに入れたり身につけて。",
+        "ふと口をついて出た曲をダウンロードするか、ラジオ番組にリクエストメールを出しましょう。",
+        "今日だけ、自分の顔の好きなところにペンシルでほくろを描いてみて。",
+    ]
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
+    
    
     @IBAction func numSlider(_ sender: UISlider) {
         sliderNum = Int(sender.value)
@@ -31,7 +58,7 @@ class ViewController: UIViewController {
     
     @IBAction func fortuneButton(_ sender: UIButton) {
         print(sliderNum)
-    
+        print(dataPicker.date)
         
     }
     
