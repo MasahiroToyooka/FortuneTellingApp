c57325cb21c4de7365fcfcf34343fc4d6ece7155
MasahiroToyooka <ehara2481@gmail.com>
2019-08-08 12:42:53 +0900

    コンポーネントの紐付け

--- FortuneTellingApp/ViewController.swift
+++ FortuneTellingApp/ViewController.swift
@@ -10,11 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var dataPicker: UIDatePicker!
+    
+    @IBOutlet weak var numLabel: UILabel!
+    
+    @IBOutlet weak var resultTextView: UITextView!
+    
+    var sliderNum: Int = 50
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+   
+    @IBAction func numSlider(_ sender: UISlider) {
+        sliderNum = Int(sender.value)
+        numLabel.text = String(sliderNum)
+    }
+    
+    
+    @IBAction func fortuneButton(_ sender: UIButton) {
+        print(sliderNum)
+    
+        
+    }
+    
 }
 
